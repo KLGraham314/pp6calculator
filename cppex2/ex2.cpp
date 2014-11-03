1557eb6ad258d7d10f145df14fb961c19040f270
Katie Graham <kg@hep.ph.bham.ac.uk>
2014-11-03 15:57:44 +0000

    Write operations program Ex 3

--- cppex2/ex2.cpp
+++ cppex2/ex2.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 int main()
 {
-	double a, b, c;
+	double a, b, c; //Declare number variables
+	char *op; //Declare char variable
 
-	std::cin >> a >> b;
+	std::cout << "Enter two numbers separated by the enter key" << std::endl; //Ask the user to enter numbers
 
-	c = a*b;
+	std::cin >> a >> b; //Take user input
 
-	std::cout << c;
+	std::cout << "What kind of operation do you want to perform? (type 'm' for multiplication, 'd' for division, 'a' for addition or 's' for subtraction, followed by the enter key" << std::endl; //Ask the user to choose operation
+
+	std::cin >> op; //Take in user input
+
+	if(!std::cin){
+		std::cout << "Input failed" << std::endl;
+	} else if(*op=='m'){
+		c = a*b;
+	} else if(*op=='d'){
+		if(b==0){
+			std::cout << "Divide by zero error" << std::endl;
+		} else{
+			c=a/b;
+		}
+	} else if(*op=='a'){
+		c=a+b;
+	} else if(*op=='s'){
+		c=a-b;
+	} else {
+		std::cout << "User input must be m, d, a or s only" << std::endl;
+	}
+
+
+	std::cout << c << std:: endl;
 
 	return 0;
 }
