114c9b7dd2210691317c9ad3fc97bd38968cdf01
Katie Graham <kg@hep.ph.bham.ac.uk>
2014-11-10 11:32:37 +0000

    Pass by reference

--- cpphw/cpphw.cpp
+++ cpphw/cpphw.cpp
@@ -17,11 +17,9 @@ double subtract(double a, double b){ //Function to subtract the two inputs
 double intercept(double a, double b){ //Function to calculate the x-intercept of y = a*x + b
 	return (-1)*b/a;
 }
-double quadraticplus(double a, double b, double d){ //Function to find the larger real solution to a quadratic equation
-	return (((-1)*b)+ sqrt((b*b)-(4*a*d)))/(2*a);
-}
-double quadraticminus(double a, double b, double d){ //Function to find the smaller real solution to a quadratic equation
-	return (((-1)*b)- sqrt((b*b)-(4*a*d)))/(2*a);
+void quadratic(double a, double b, double d, double& c, double & cminus){ //Function to find the solutions to a quadratic equation
+	c = (((-1)*b)+ sqrt((b*b)-(4*a*d)))/(2*a);
+	cminus = (((-1)*b) - sqrt((b*b)-(4*a*d)))/(2*a);
 }
 double tlength(double a, double b, double d){ //Function to find the length of a 3-vector
 	double sumsqr = (a*a) + (b*b) + (d*d);
@@ -29,13 +27,13 @@ double tlength(double a, double b, double d){ //Function to find the length of a
 }
 double flength(double a, double b, double d, double e){ //Function to find the length of a 4-vector
 	double sumsqr = (a*a) - (b*b) - (d*d) - (e*e);
-	if(sumsqr>=0) return sqrt(sumsqr);
-	if(sumsqr<0) return sqrt(-1*sumsqr);
+	 return sqrt(std::abs(sumsqr));
 }
-double invmass(double E1, double E2, double px1, double px2, double py1, double py2, double pz1, double pz2){ //Function to calculate invariant mass of two particles
+bool invmass(double E1, double E2, double px1, double px2, double py1, double py2, double pz1, double pz2, double& c){ //Function to calculate invariant mass of two particles
 	double sumsqr = ((E1+E2)*(E1+E2)) - ((px1+px2)*(px1+px2)) - ((py1+py2)*(py1+py2)) - ((pz1+pz2)*(pz1+pz2));
-	if(sumsqr>=0) return sqrt(sumsqr);
-	if(sumsqr<0) return -1; // E^2<p^2 is unphysical
+	c = sqrt(sumsqr);
+	if(sumsqr>=0) return 1; // In this case 1 = "true" = "success"
+	if(sumsqr<0) return 0; // In this case 0 = "false" = "failure"
 }
 
 int main()
@@ -46,7 +44,7 @@ int main()
 	std::cout << "Welcome to the PP6 calculator!" << std::endl;
 
 	while(j==0){ //While j remains zero, the calculator will keep performing functions until user quits
-		std::cout << "What kind of operation do you want to perform? (type 'm' for multiplication, 'd' for division, 'a' for addition or 's' for subtraction, 'x' to calculate the x-intercept of a line, 'e' to solve a quadratic equation, 't' to calculate the length of a 3-vector, 'p' to calculate the invariant mass of two particles, or 'q' for quit, followed by the enter key)" << std::endl; //Ask the user to choose operation
+		std::cout << "What kind of operation do you want to perform? (type 'm' for multiplication, 'd' for division, 'a' for addition or 's' for subtraction, 'x' to calculate the x-intercept of a line, 'e' to solve a quadratic equation, 't' to calculate the length of a 3-vector, 'f' to calculate the length of a 4-vector, 'p' to calculate the invariant mass of two particles, or 'q' for quit, followed by the enter key)" << std::endl; //Ask the user to choose operation
 		std::cin >> op; //Take in user selection
 
 		if(op=='q'){ break;} //User quits - end program
@@ -122,8 +120,7 @@ int main()
 		} else if(op=='e'){ //Solve quadratic equation
 			inside = (b*b)-(4*a*d); //Evaluate b^2 - 4*a*c
 			if(inside>=0){ //If inside is positive the answer will be real
-				c=quadraticplus(a,b,inside);
-				cminus=quadraticminus(a,b,inside);
+				quadratic(a,b,inside,c,cminus);
 			}else{ //If inside is negative the answer will be imaginary
 				c = (-1*b)/(2*a); //Real part
 				cminus = sqrt(-1*inside)/(2*a); //Imaginary part
@@ -133,8 +130,8 @@ int main()
 		} else if(op=='f'){ //Find length of 4-vector
 			c = flength(a,b,d,e);
 		} else if(op=='p'){ //Find invariant mass of two particles
-			c = invmass(E1,E2,px1,px2,py1,py2,pz1,pz2);
-			if(c<0){ //If E^2<p^2, c will be -1
+			bool real = invmass(E1,E2,px1,px2,py1,py2,pz1,pz2,c);
+			if(real==0){ //If E^2<p^2, invmass will return 0
 				std::cout << "E^2 < p^2 for these particles, which is unphysical" << std::endl;
 				continue;
 			}	
